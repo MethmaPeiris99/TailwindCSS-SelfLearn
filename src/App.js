707e9a6898a82e77c135fdd151e406e7c0ede6f5
MethmaPeiris99 <methma34@gmail.com>
2023-09-27 23:24:36 +0530

    Introduction to @apply directive

--- src/App.js
+++ src/App.js
@@ -45,13 +45,35 @@ function App() {
           <h4 className='font-bold mt-12 pb-2 border-b border-gray-200'>Latest Recipes</h4>
 
           <div className='mt-8'>
-            <div className='bg-white rounded overflow-hidden shadow relative'>
+            <div className='card'>
               <img src="images/stew.jpg" alt="stew" className='w-full h-32 sm:h-48 object-cover'/>
               <div className='m-4'>
                 <span className='font-bold'>5 Bean Chilli Stew</span>
                 <span className='block text-gray-500 text-sm'>Recipe by Mario</span>
               </div>
-              <div className='bg-secondary-100 text-secondary-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2'>
+              <div className='badge'>
+                <span>25 mins</span>
+              </div>
+            </div>
+
+            <div className='card'>
+              <img src="images/noodles.jpg" alt="noodles" className='w-full h-32 sm:h-48 object-cover'/>
+              <div className='m-4'>
+                <span className='font-bold'>Veg Noodles</span>
+                <span className='block text-gray-500 text-sm'>Recipe by Mario</span>
+              </div>
+              <div className='badge'>
+                <span>25 mins</span>
+              </div>
+            </div>
+
+            <div className='card'>
+              <img src="images/curry.jpg" alt="curry" className='w-full h-32 sm:h-48 object-cover'/>
+              <div className='m-4'>
+                <span className='font-bold'>Tofu Curry</span>
+                <span className='block text-gray-500 text-sm'>Recipe by Mario</span>
+              </div>
+              <div className='badge'>
                 <span>25 mins</span>
               </div>
             </div>
