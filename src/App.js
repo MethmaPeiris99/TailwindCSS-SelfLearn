5645f5bf1084fd138e8872ed9f177180f3671d48
MethmaPeiris99 <methma34@gmail.com>
2023-09-27 23:08:07 +0530

    Style the card

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         </nav>
       </div>
 
-      <main className='px-16 py-6'>
+      <main className='px-16 py-6 bg-gray-100'>
         <div className='flex justify-center md:justify-end'>
           <a href="#" className='text-primary'>Login</a>
           <a href="#" className='text-primary ml-2'>Signup</a>
@@ -45,11 +45,11 @@ function App() {
           <h4 className='font-bold mt-12 pb-2 border-b border-gray-200'>Latest Recipes</h4>
 
           <div className='mt-8'>
-            <div>
-              <img src="images/stew.jpg" alt="stew"/>
-              <div>
-                <span>5 Bean Chilli Stew</span>
-                <span>Recipe by Mario</span>
+            <div className='bg-white rounded overflow-hidden shadow'>
+              <img src="images/stew.jpg" alt="stew" className='w-full h-32 sm:h-48 object-cover'/>
+              <div className='m-4'>
+                <span className='font-bold'>5 Bean Chilli Stew</span>
+                <span className='block text-gray-500 text-sm'>Recipe by Mario</span>
               </div>
             </div>
           </div>
